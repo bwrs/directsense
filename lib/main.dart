7c9138f9582ca331a639a4886e7c506e649c1e44
KernelPannick <flumpyelephant@gmail.com>
2018-06-11 00:27:26 +0100

    Add central testing screen and vibrational alert functionality.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import "package:flutter/services.dart";
+import "dart:async";
 
 void main() => runApp(new MyApp());
 
@@ -7,103 +9,163 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Component Testing',
       theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
+        brightness: Brightness.dark,
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+      home: new TestSelectionStateful(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class TestSelectionStateful extends StatefulWidget {
   @override
-  _MyHomePageState createState() => new _MyHomePageState();
+  createState() => new TestSelection();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class TestSelection extends State<TestSelectionStateful> {
+  //TestSelection({Key key, this.title}) : super(key: key);
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  // This widget is the test selection page.
+  // It is stateless.
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return new Scaffold(
-      appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title),
-      ),
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
-            ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+        appBar: new AppBar(
+            title: new Text("Test Selection")
         ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        body: new ListView(
+            children: ListTile.divideTiles(
+                context: context,
+                tiles: <Widget>[
+                  ListTile(
+                      leading: new Icon(Icons.vibration),
+                      title: new Text("Haptic Feedback"),
+                      onTap: () {
+                        pushHaptic(context);
+                      }
+                  )
+                ]
+            ).toList()
+        )
     );
+  }
+
+  void pushHaptic(context) {
+    Navigator.of(context).push(
+        new MaterialPageRoute(
+            builder: (context) {
+              return new Scaffold(
+                  appBar: new AppBar(
+                      title: new Text("Haptic Feedback Tests")
+                  ),
+                  body: new ListView(
+                      children: ListTile.divideTiles(
+                          context: context,
+                          tiles: <Widget>[
+                            ListTile(
+                                title: new Text("Custom 100ms"),
+                                onTap: () {
+                                  GoodVibrations.vibrate(100);
+                                }
+                            ),
+                            ListTile(
+                                title: new Text("Custom 300ms"),
+                                onTap: () {
+                                  GoodVibrations.vibrate(250);
+                                }
+                            ),
+                            ListTile(
+                                title: new Text("Custom 500ms"),
+                                onTap: () {
+                                  GoodVibrations.vibrate(500);
+                                }
+                            ),
+                            ListTile(
+                                title: new Text("SOS"),
+                                onTap: () {
+                                  GoodVibrations.vibrateWithPauses([
+                                    100, 100, 100, 100, 100, 300,
+                                    300, 100, 300, 100, 300, 300,
+                                    100, 100, 100, 100, 100
+                                  ]);
+                                }
+                            ),
+                            ListTile(
+                                title: new Text("Alert 12345 67890"),
+                                onTap: () {
+                                  GoodVibrations.alert("12345 67890");
+                                }
+                            )
+                          ]
+                      ).toList()
+                  )
+              );
+            }
+        )
+    );
+  }
+}
+
+class GoodVibrations {
+  static const MethodChannel _channel = const MethodChannel(
+      'github.com/clovisnicolas/flutter_vibrate');
+
+  static Future vibrate(ms) =>
+      _channel.invokeMethod("vibrate", {"duration": ms});
+
+  ///Take in an Iterable<int> of the form
+  ///[l_1, p_1, l_2, p_2, ..., l_n]
+  ///then vibrate for l_1 ms,
+  ///pause for p_1 ms,
+  ///vibrate for l_2 ms,
+  ///...
+  ///and vibrate for l_n ms.
+  static Future vibrateWithPauses(Iterable<int> periods) async {
+    bool isVibration = true;
+    for (int d in periods) {
+      if (isVibration && d > 0) {
+        vibrate(d);
+      }
+      await new Future.delayed(Duration(milliseconds: d));
+      isVibration = !isVibration;
+    }
+  }
+
+  static const BinaryMap = {
+    " ": [0, 1200],
+    "0": [75, 1125],
+    "1": [225, 975],
+    "2": [225, 75, 75, 825],
+    "3": [225, 75, 225, 675],
+    "4": [225, 75, 75, 75, 75, 675],
+    "5": [225, 75, 75, 75, 225, 525],
+    "6": [225, 75, 225, 75, 75, 525],
+    "7": [225, 75, 225, 75, 225, 375],
+    "8": [225, 75, 75, 75, 75, 75, 75, 525],
+    "9": [225, 75, 75, 75, 75, 75, 225, 375]
+  };
+
+  static const BinaryMap50_150 = {
+    " ": [0, 1000],
+    "0": [50, 950],
+    "1": [150, 850],
+    "2": [150, 50, 50, 750],
+    "3": [150, 50, 150, 650],
+    "4": [150, 50, 50, 50, 50, 650],
+    "5": [150, 50, 50, 50, 150, 550],
+    "6": [150, 50, 150, 50, 50, 550],
+    "7": [150, 50, 150, 50, 150, 450],
+    "8": [150, 50, 50, 50, 50, 50, 50, 550],
+    "9": [150, 50, 50, 50, 50, 50, 150, 450]
+  };
+
+  static void alert(String data) {
+    List<int> periods = [];
+    for (int i = 0; i < data.length; i++) {
+      periods.addAll(BinaryMap[data[i]]);
+    }
+    vibrateWithPauses(periods);
   }
 }
